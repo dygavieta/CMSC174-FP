92eef43aea07d4c6cc938261848ff354ecf65d20
John Honeyman <johnhoneyman10@gmail.com>
2022-05-28 15:43:47 +0800

    added more layouts

--- elements/testgui.py
+++ elements/testgui.py
@@ -1,19 +1,31 @@
 import PySimpleGUI as sg
 import os.path
+from PIL import Image 
+
+answer_keys = open('answer_keys/answer_keys.txt','r')
+with answer_keys as f_in:
+    answers = (answer.rstrip() for answer in f_in) 
+    answers = list(answer for answer in answers if answer)
+answer_keys.close()
+
+score = 0
 
 file_list_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size = (25,1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(), 
-    ],
-    [
+    [sg.Button("Scan", size =(40,2))],
+    [sg.Button("Save", size =(40,2))],   
+    [sg.FolderBrowse("Open", size =(40,2),enable_events=True, key="-FOLDER-")],
+    [sg.Text("Answers:")],
+]
+for i in range(len(answers)):
+    file_list_column += [[sg.Text(f"{i+1}.", pad=(0,0)), sg.Text(answers[i].upper(),pad=(0,0))]]
+
+file_list_column +=  [
         sg.Listbox(
-            values=[], enable_events=True, size=(40,20),
+            values=[], enable_events=True, size=(40,10),
             key = "-FILE LIST-"
         )
-    ]
-]
+    ],
+file_list_column +=[[sg.Text("Score: ", font=("Any 16")), sg.Text(f"{score}", font=("Any 16"))]]
 
 image_viewer_column = [
     [sg.Text("Choose an image from the list" )],
@@ -35,6 +47,11 @@ while True:
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
+    if event == "Scan":
+        print("Scanning...")
+    if event == "Save":
+        print("Saving...")
+        sg.popup('Saved!', title="Saved image")
     if event == "-FOLDER-":
         folder = values["-FOLDER-"]
         try:
@@ -43,10 +60,10 @@ while True:
             file_list = []
 
         fnames = [
-            f
-            for f in file_list
-            if os.path.isfile(os.path.join(folder, f))
-            and f.lower().endswith((".png",".gif"))
+            file
+            for file in file_list
+            if os.path.isfile(os.path.join(folder, file))
+            and file.lower().endswith((".png",".gif"))
         ]
         window["-FILE LIST-"].update(fnames) 
     elif event == "-FILE LIST-":
@@ -54,8 +71,12 @@ while True:
             filename = os.path.join(
                 values["-FOLDER-"], values["-FILE LIST-"][0]
             )
+            print(filename)
             window["-TOUT-"].update(filename)
             window["-IMAGE-"].update(filename=filename)
         except:
             pass
+
+
+
 window.close()
